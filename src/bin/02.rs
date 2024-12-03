65a12ddd728033c59c6f6d02a39c7ae7a8529520
Thibault Douzon <douzont@gmail.com>
2024-12-03 21:21:00 +0100

    second day

---
+++ src/bin/02.rs
@@ -0,0 +1,70 @@
+advent_of_code::solution!(2);
+
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn is_valid(report: &Vec<i32>) -> bool {
+    let direction = (report[1] - report[0]).signum();
+    if direction == 0 {
+        return false;
+    }
+
+    for (x, y) in report.iter().zip(report.iter().skip(1)) {
+        if (y - x).signum() != direction {
+            return false;
+        }
+        if (y - x).abs() > 3 {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let reports = parse_input(input);
+    Some(reports.iter().filter(|&report| is_valid(report)).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let reports = parse_input(input);
+    Some(
+        reports
+            .iter()
+            .filter(|&report| {
+                if is_valid(report) {
+                    return true;
+                }
+                (0..report.len()).any(|i| {
+                    let mut report_small = report.clone();
+                    report_small.remove(i);
+                    is_valid(&report_small)
+                })
+            })
+            .count() as u32,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
